68a31936b3137409a4a708978568ead7b1f93221
William Koza <william.koza@gmail.com>
2019-08-01 16:00:08 +0200

    add application ngx-uploader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,56 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+import {
+    DropTargetOptions, NgxUploadModule
+} from '@wkoza/ngx-upload';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SimpleBootstrapComponent } from './bootstrap/simple.component';
+import { SimpleMaterialComponent } from './material/simple.component';
+import { HttpClientModule } from '@angular/common/http';
+import {
+    MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, MatProgressBarModule
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const routes: Routes = [
+    {path: '', redirectTo: 'simple-bootstrap', pathMatch: 'full'},
+    {path: 'simple-bootstrap', component: SimpleBootstrapComponent},
+    {path: 'simple-material', component: SimpleMaterialComponent}
+];
+
+export const ngxDropTargetOptions: DropTargetOptions = {
+    color: 'dropZoneColor',
+    colorDrag: 'dropZoneColorDrag',
+    colorDrop: 'dropZoneColorDrop'
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        SimpleBootstrapComponent,
+        SimpleMaterialComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        RouterModule.forRoot(routes, {useHash: true}),
+        NgxUploadModule.forRoot(ngxDropTargetOptions),
+        NgbModule,
+        BrowserAnimationsModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        MatCardModule,
+        MatProgressBarModule,
+        MatInputModule
+    ],
+
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
