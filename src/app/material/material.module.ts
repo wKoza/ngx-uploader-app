59519968118d0b36730a3c1fe8644dec48a25532
William Koza <william.koza@gmail.com>
2020-08-04 10:55:32 +0200

    chore: update angular 10

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,10 @@
 import {NgModule} from '@angular/core';
 
-import { MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, MatProgressBarModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { SimpleMaterialComponent } from './simple.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
