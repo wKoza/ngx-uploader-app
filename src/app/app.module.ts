6c45aa4e732f8e7692ae4425594ce05a0e12ecf9
William Koza <william.koza@gmail.com>
2020-09-04 09:22:07 +0200

    add HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import {
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
     {path: '', redirectTo: 'simple-material', pathMatch: 'full'},
@@ -26,6 +27,7 @@ export const ngxDropTargetOptions: DropTargetOptions = {
     ],
     imports: [
       BrowserModule,
+      HttpClientModule,
       BrowserAnimationsModule,
       RouterModule.forRoot(routes),
       NgxUploadModule.forRoot(ngxDropTargetOptions)
